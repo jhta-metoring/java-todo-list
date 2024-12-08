55756b1e0401b5866f3a82d0ce01bb73a8f0aaad
bitnalchan92 <bitnalchan92@gmail.com>
2024-12-08 10:04:52 +0900

    [feat] 요구사항 2, 3, 4 완료 및 소스 일부 정리

--- src/main/java/org/homework/Main.java
+++ src/main/java/org/homework/Main.java
@@ -1,36 +1,49 @@
 package org.homework;
 
+import org.homework.func.Common;
+import org.homework.func.Todo;
+
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        HashMap<Integer, String> todos = new HashMap<>();
-        Option[] options = Option.values();
-
-        String validPattern = "^[1-" + options.length + "]$";
-        Pattern pattern = Pattern.compile(validPattern);
+        HashMap<Integer, String> todos = new HashMap<>(); // 할 일 목록을 관리할 HashMap
+        Scanner scanner = new Scanner(System.in); // Scanner 객체
 
         int selectedOption = 0;
-        while (selectedOption != 4) {
-            System.out.print("옵션을 선택하세요: ");
-
-            for (int i = 0; i < options.length; i++) {
-                Option option = options[i];
-                String format = i < options.length - 1 ? "%s.%s, " : "%s.%s ~> ";
-                System.out.printf(format, option.getOptionNum(), option.getOptionName());
-            }
+        while (selectedOption != Option.EXIT.getOptionNum()) {
+            Option.printOptions(); // 사용자에게 안내 출력 ( 옵션을 선택하세요... )
 
             String inputValue = scanner.nextLine();
-            Matcher matcher = pattern.matcher(inputValue);
 
-            if ( matcher.matches() ) {
+            if (Common.regexNumCheck(inputValue)) {
                 selectedOption = Integer.parseInt(inputValue);
-                // TODO: 2024/12/08
+
+                // 정규식에는 통과했지만 해당 번호가 Option에서 관리중인 유효한 옵션 넘버인지 확인
+                boolean isValidOptionNum = false;
+                for (int validOption : Option.getIntArrOfOptionNum()) {
+                    if (selectedOption == validOption) {
+                        isValidOptionNum = true;
+                        break;
+                    }
+                }
+
+                if (isValidOptionNum) {
+                    switch (Option.getOptionByNum(selectedOption)) {
+                        case ADD:
+                            Todo.addTodo(scanner, todos);
+                            break;
+                        case DELETE:
+                            Todo.deleteTodo(scanner, todos);
+                            break;
+                        case VIEW:
+                            Todo.viewTodo(scanner, todos);
+                            break;
+                    }
+                } else {
+                    System.out.println("선택 불가능한 옵션 번호입니다.");
+                }
             } else {
                 System.out.println("잘못된 입력입니다.");
             }
--- src/main/java/org/homework/Option.java
+++ src/main/java/org/homework/Option.java
@@ -22,15 +22,52 @@ public enum Option {
         return optionName;
     }
 
-    public static Option getOptionNameByNum(int optionNum) {
+    /**
+     * Scanner로부터 옵션 번호를 입력 받아 그에 해당하는 Option객체를 반환하는 함수
+     *
+     * @param optionNum Scanner로 입력받은 inputValue값
+     * @return 번호에 해당하는 Option객체
+     */
+    public static Option getOptionByNum(int optionNum) {
+        Option result = null;
+
         for (Option option : Option.values()) {
-            System.out.println("===> option : ");
-            System.out.println(option);
             if (option.getOptionNum() == optionNum) {
-                return option;
+                result = option;
             }
         }
 
-        return null;
+        return result;
+    }
+
+    /**
+     * Option에 등록되어 있는 메뉴들의 번호를 배열로 반환하는 함수 ( 옵션 번호가 순차적으로 등록되지 않을 가능성이 있어 그러한 경우를 비교하기 위해 사용함 )
+     *
+     * @return int[]
+     */
+    public static int[] getIntArrOfOptionNum() {
+        Option[] options = Option.values();
+        int[] validOptionNumbers = new int[options.length];
+        for (int i = 0; i < options.length; i++) {
+            validOptionNumbers[i] = options[i].getOptionNum();
+        }
+
+        return validOptionNumbers;
+    }
+
+    /**
+     * 사용자가 선택 가능한 메뉴 목록을 콘솔에 출력하는 함수
+     */
+    public static void printOptions() {
+        Option[] options = Option.values();
+
+        System.out.print("옵션을 선택하세요: ");
+
+        for (int i = 0; i < options.length; i++) {
+            Option option = options[i];
+            String format = i < options.length - 1 ? "%s.%s, " : "%s.%s ~> ";
+
+            System.out.printf(format, option.getOptionNum(), option.getOptionName());
+        }
     }
 }
