ecd68df2b43d561f1e93a97f6ea7b3f0699579e2
ChoiGoeunn <rhdms085@gamil.com>
2024-12-17 21:22:20 +0900

    [feat] 1단계 수정

--- src/main/java/org/homework/TodoList.java
+++ src/main/java/org/homework/TodoList.java
@@ -5,30 +5,31 @@ import java.util.Scanner;
 
 public class TodoList {
     public static void main(String[] args) {
-        HashMap<Integer, String> list = new HashMap<>();
+        HashMap<Integer, String> todoList = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         int count = 1;
 
         while (true) {
             System.out.println("옵션을 선택하세요: 1. 추가, 2. 삭제, 3. 조회, 4. 종료");
             String input = scanner.nextLine();
+            EnumClass operation = EnumClass.fromInput(input);
 
-            switch (input) {
-                case "1":
+            switch (operation) {
+                case Add:
                     System.out.print("추가할 할 일을 입력하세요: ");
-                    String addList = scanner.nextLine();
-                    list.put(count, addList);
+                    String todo = scanner.nextLine();
+                    todoList.put(count, todo);
                     System.out.println("할 일이 추가되었습니다. ID: " + count);
                     count++;
                     break;
 
-                case "2":
+                case Delete:
                     System.out.print("삭제할 할 일의 ID를 입력하세요: ");
                     try {
                         int deleteId = Integer.parseInt(scanner.nextLine());
-                        if (list.get(deleteId) != null) {
-                            System.out.println("삭제된 할 일: " + list.get(deleteId));
-                            list.remove(deleteId);
+                        if (todoList.containsKey(deleteId)) {
+                            System.out.println("삭제된 할 일: " + todoList.get(deleteId));
+                            todoList.remove(deleteId);
                         } else {
                             System.out.println("해당 ID의 할 일이 없습니다.");
                         }
@@ -37,13 +38,12 @@ public class TodoList {
                     }
                     break;
 
-                case "3":
+                case Select:
                     System.out.print("조회할 할 일의 ID를 입력하세요: ");
                     try {
-                        int selectId = Integer.parseInt(scanner.nextLine());
-                        String toDo = list.get(selectId);
-                        if (toDo != null) {
-                            System.out.println("할 일 ID: " + selectId + " 내용: " + toDo);
+                        int viewId = Integer.parseInt(scanner.nextLine());
+                        if (todoList.containsKey(viewId)) {
+                            System.out.println("할 일 ID: " + viewId + " 내용: " + todoList.get(viewId));
                         } else {
                             System.out.println("해당 ID의 할 일이 없습니다.");
                         }
@@ -52,13 +52,17 @@ public class TodoList {
                     }
                     break;
 
-                case "4":
+                case Exit:
                     System.out.println("프로그램을 종료합니다.");
                     scanner.close();
                     return;
 
+                case False:
+                    System.out.println("잘못된 입력입니다. 다시 시도하세요.");
+                    break;
+
                 default:
-                    System.out.println("잘못된 입력입니다.");
+                    System.out.println("알 수 없는 오류가 발생했습니다.");
                     break;
             }
         }
